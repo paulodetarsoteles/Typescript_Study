297cfd038b86c712c3ca92c7114985fa9547176e
Paulo de Tarso <potassio@hotmail.com>
2023-10-22 17:58:31 -0300

    Mais adições de exemplos de variáveis

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,10 +1,15 @@
 "use strict";
 //variável em javascript normal
-const varJs1 = 10;
+const varJs1 = 1;
 const varJs2 = true;
 const varJs3 = "teste";
+console.log(varJs1 + ". " + varJs2 + " - " + varJs3);
 //variável em typescript (string, boolean, number...)
 let varTs1 = 1;
 const varTs2 = true;
 const varTs3 = "teste";
+const varTs4 = [1, 2, 3];
 console.log(varTs1 + ". " + varTs2 + " - " + varTs3);
+console.log(varTs3.toUpperCase());
+console.log(typeof varTs3);
+console.log(varTs4 + " - " + varTs4.length);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
+
 //variável em javascript normal
-const varJs1 = 10;
+
+const varJs1 = 1;
 const varJs2 = true;
 const varJs3 = "teste";
+console.log(varJs1 + ". " + varJs2 + " - "+ varJs3);
 
 //variável em typescript (string, boolean, number...)
+
 let varTs1: number = 1;
 const varTs2: boolean = true;
 const varTs3: string = "teste";
+const varTs4: number[] = [1, 2, 3];
 
 console.log(varTs1 + ". " + varTs2 + " - "+ varTs3);
+console.log(varTs3.toUpperCase);
+console.log(typeof varTs3);
+console.log(varTs4 + " - " + varTs4.length);

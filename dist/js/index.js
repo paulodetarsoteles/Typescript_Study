659c75a9979148947af78d2eeadfca4f33a5ccc5
Paulo de Tarso <potassio@hotmail.com>
2023-10-22 17:36:20 -0300

    Primeira compilação do projeto

---
+++ dist/js/index.js
@@ -0,0 +1,10 @@
+"use strict";
+//variável em javascript normal
+const varJs1 = 10;
+const varJs2 = true;
+const varJs3 = "teste";
+//variável em typescript (string, boolean, number...)
+let varTs1 = 1;
+const varTs2 = true;
+const varTs3 = "teste";
+console.log(varTs1 + ". " + varTs2 + " - " + varTs3);

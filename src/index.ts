0cee291c52e0980645a563d54382d0bef07d12fb
Paulo de Tarso <potassio@hotmail.com>
2023-10-22 17:28:41 -0300

    Criação do projeto, do arquivo html, do arquivo ts, da inicialização do tsc

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+//variável em javascript normal
+const varJs1 = 10;
+const varJs2 = true;
+const varJs3 = "teste";
+
+//variável em typescript (string, boolean, number...)
+let varTs1: number = 1;
+const varTs2: boolean = true;
+const varTs3: string = "teste";
+
+console.log(varTs1 + ". " + varTs2 + " - "+ varTs3);
